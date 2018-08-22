8fbb42879543efb39c4994291a586d0e48c2d84c
beatrizelena <ortizwebdevelopment@gmail.com>
2018-08-22 05:58:46 -0400

    Work-in-Progress: Add phone validator

--- app/validators/phone.js
+++ app/validators/phone.js
@@ -1,8 +1,10 @@
-import validate from 'mongoose-validator';
+// const validator = ('mongoose-validator')
 
-export default validate({
-  validator: 'matches',
-  arguments: /^\(?[0-9]{3}\)?[-.\/\s]?[0-9]{3}[-.\/\s]?[0-9]{4}$/,
-  passIfEmpty: true,
-  message: 'Must be a properly formatted phone number for the United States'
-});
+// const validate = new validator({
+//   validator: 'matches',
+//   arguments: /^\(?[0-9]{3}\)?[-.\/\s]?[0-9]{3}[-.\/\s]?[0-9]{4}$/,
+//   passIfEmpty: true,
+//   message: 'Must be a properly formatted phone number for the United States'
+// });
+
+// module.exports = validate
