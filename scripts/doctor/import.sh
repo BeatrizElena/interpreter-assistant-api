3ec2c6e76ec37225e341466fbf24d71d055448e4
beatrizelena <ortizwebdevelopment@gmail.com>
2018-09-05 10:46:55 -0400

    Add mongoimport script to import clinic collection

--- scripts/doctor/import.sh
+++ scripts/doctor/import.sh
@@ -1,2 +1,4 @@
 # sh scripts/doctor/import.sh
 mongoimport --db=heroku_7f9l1c7n --collection=doctors --type=csv --headerline --file=data/doctors.csv
+mongoimport --db=heroku_7f9l1c7n --collection=clinics --type=csv --headerline --file=data/clinics.csv
+mongoimport --db=heroku_7f9l1c7n --collection=diseases --type=csv --headerline --file=data/diseases.csv
