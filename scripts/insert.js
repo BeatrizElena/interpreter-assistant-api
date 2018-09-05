d3f2dcb75ba1f85be2cfaf19a65b6530b5d6728c
beatrizelena <ortizwebdevelopment@gmail.com>
2018-09-05 16:43:51 -0400

    Create file for db.doctors.insert() and db.clinics.insert

---
+++ scripts/insert.js
@@ -0,0 +1,12 @@
+db.clinics.insert( {abbreviation: "HEO", name: "Hematologic Oncology", phone: "617-123-1234", description: "All types of cancers of the blood such as leukemia and lymphoma. The center also includes the bone marrow/stem cell transplantation program."} )
+db.clinics.insert( {abbreviation: "NEO", name: "Neuro Oncology", phone: "617-123-1234", description: "Brain tumors and spinal cord tumors. Treat both common and rare brain tumors such as glioblastoma and oligodendroglioma."} )
+db.clinics.insert( {abbreviation: "JF", name: "Jimmy Fund/Pediatric Outpatient", phone: "617-123-1234", description: "The Jimmy Fund Clinic is the outpatient clinic of the Dana Farber/Boston Children's integrated center that specializes in pediatric cancer and blood disorders. Specific therapies may include surgery and chemotherapy as well as stem cell transplant."} )
+
+
+db.doctors.insert( { first_name: "Jorge", last_name: "Castillo", phone: "777-888-9999", title: "MD", clinic: "5b90342a927eeb9572e659cd" } )
+db.doctors.insert( { first_name: "Irene", last_name: "Ghobrial", phone: "777-888-9999", title: "MD", clinic: "5b90342a927eeb9572e659cd" } )
+db.doctors.insert( { first_name: "Steven P.", last_name: "Treon", phone: "777-888-9999", title: "MD", clinic: "5b90342a927eeb9572e659cd" } )
+db.doctors.insert( { first_name: "Beatriz", last_name: "Ortiz", phone: "777-888-9999", title: "MD PhD", clinic: "5b903428927eeb9572e659cc" } )
+db.doctors.insert( { first_name: "Maria", last_name: "Perez", phone: "777-888-9999", title: "MD", clinic: "5b903456927eeb9572e659ce" } )
+db.doctors.insert( { first_name: "Ava", last_name: "Gil", phone: "777-888-9999", title: "MD PhD", clinic: "5b903469927eeb9572e659cf" } )
+
